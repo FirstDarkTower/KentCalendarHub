4af38fbf2d8747d4b07367abccf040aee8b2860a
Jarrek R. Holmes <jholmes16@kentdenver.org>
2014-08-06 10:46:27 -0600

    This is updated with organized selectors, empty brackets for offs, and set except for teacher and ID's. Have you found a way to talk to the database yet?? Is there something I need to do for middle school?

--- StudentSite/views.py
+++ StudentSite/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from StudentSite.models import Calendar
 
-
 def index(request):
     context = RequestContext(request)
     context_list = dict(period_1_cals=Calendar.objects.filter(period=1))
-    context_list["period_2_cals"] = Calendar.objects.filter(period = 2)
-    context_list["period_3_cals"] = Calendar.objects.filter(period = 3)
-    context_list["period_4_cals"] = Calendar.objects.filter(period = 4)
-    context_list["period_5_cals"] = Calendar.objects.filter(period = 5)
-    context_list["period_6_cals"] = Calendar.objects.filter(period = 6)
-    context_list["period_7_cals"] = Calendar.objects.filter(period = 7)
-    return render_to_response("StudentSite/index.html", context, context_list)
+    context_list["period_1_cals"] = context_list["period_1_cals"].order_by('class_title')
+    context_list["period_2_cals"] = Calendar.objects.filter(period = 2).order_by('class_title')
+    context_list["period_3_cals"] = Calendar.objects.filter(period = 3).order_by('class_title')
+    context_list["period_4_cals"] = Calendar.objects.filter(period = 4).order_by('class_title')
+    context_list["period_5_cals"] = Calendar.objects.filter(period = 5).order_by('class_title')
+    context_list["period_6_cals"] = Calendar.objects.filter(period = 6).order_by('class_title')
+    context_list["period_7_cals"] = Calendar.objects.filter(period = 7).order_by('class_title')
+
+    return render_to_response("StudentSite/index.html", context_list, context)
+
+
 
