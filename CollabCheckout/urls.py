e2cd9adebc53f64ff07335d0f168d65e38ef4b6b
Alex Clement <aclement@kentdenver.org>
2014-08-20 23:14:31 -0600

    Mostly finished collab checkout

--- CollabCheckout/urls.py
+++ CollabCheckout/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, url
 from CollabCheckout import views
 
 urlpatterns = patterns('',
-                       url(r"^$",views.index, name = 'index'),
+                       url(r"^$",views.checkout, name = 'checkout'),
                        url(r"^period_list/$", views.period_list, name='period_list'),
                        url(r"^room_list/$", views.room_list, name='room_list'),
-                       url(r"^checkout/$", views.checkout, name='checkout'),
                        url(r"^cs/$", views.active_rooms, name='cs'),
                        )
